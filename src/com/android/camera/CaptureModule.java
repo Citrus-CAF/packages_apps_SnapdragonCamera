77cae1bc693fcb28aa58a3e0a70cfcc394dd25e0
codeworkx <daniel.hillenbrand@codeworkx.de>
2018-01-04 23:13:53 +0100

    Snap: check tags before using them
    MessageQueue-JNI: java.lang.IllegalArgumentException: Could not find tag for key
    'org.codeaurora.qcamera3.saturation.use_saturation')
    
    Change-Id: Ia42c67552f9d4574e5f86af2ac5aea853198d0ed

--- src/com/android/camera/CaptureModule.java
+++ src/com/android/camera/CaptureModule.java
@@ -5223,6 +5223,8 @@ public class CaptureModule implements CameraModule, PhotoController,
     }
 
     private void applyInstantAEC(CaptureRequest.Builder request) {
+        if (!VendorTagUtil.isSupported(request, CaptureModule.INSTANT_AEC_MODE))
+            return;
         String value = mSettingsManager.getValue(SettingsManager.KEY_INSTANT_AEC);
         if (value == null || value.equals("0"))
             return;
@@ -5231,6 +5233,8 @@ public class CaptureModule implements CameraModule, PhotoController,
     }
 
     private void applySaturationLevel(CaptureRequest.Builder request) {
+        if (!VendorTagUtil.isSupported(request, CaptureModule.SATURATION))
+            return;
         String value = mSettingsManager.getValue(SettingsManager.KEY_SATURATION_LEVEL);
         if (value != null) {
             int intValue = Integer.parseInt(value);
@@ -5315,6 +5319,8 @@ public class CaptureModule implements CameraModule, PhotoController,
     }
 
     private void applyHistogram(CaptureRequest.Builder request) {
+        if (!VendorTagUtil.isSupported(request, CaptureModule.histMode))
+            return;
         String value = mSettingsManager.getValue(SettingsManager.KEY_STATS_VISUALIZER_VALUE);
         if (value != null ) {
             if (value.equals("3")) {
