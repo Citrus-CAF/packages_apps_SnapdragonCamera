b7464d08c927a56b36dbe61a25ed8c56fa4c64e7
codeworkx <codeworkx@cyanogenmod.org>
2018-12-16 22:34:34 +0530

    Snap: Add getContext() method to CameraApp
    Change-Id: I1fe88f0467bb914316996a0279f8b0b8e76e08f0
    Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>

--- src/com/android/camera/app/CameraApp.java
+++ src/com/android/camera/app/CameraApp.java
@@ -19,6 +19,8 @@ package com.android.camera.app;
 import android.app.ActivityManager;
 import android.app.Application;
 
+import android.content.Context;
+
 import com.android.camera.SDCard;
 import com.android.camera.util.CameraUtil;
 import com.android.camera.util.UsageStatistics;
@@ -28,9 +30,12 @@ public class CameraApp extends Application {
     private static long mMaxSystemMemory;
     public static boolean mIsLowMemoryDevice = false;
     private static final long LOW_MEMORY_DEVICE_THRESHOLD = 2L*1024*1024*1024;
+    private static Application mApp = null;
+
     @Override
     public void onCreate() {
         super.onCreate();
+        mApp = this;
         ActivityManager actManager = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
         ActivityManager.MemoryInfo memInfo = new ActivityManager.MemoryInfo();
         actManager.getMemoryInfo(memInfo);
@@ -42,6 +47,10 @@ public class CameraApp extends Application {
         UsageStatistics.initialize(this);
         CameraUtil.initialize(this);
         SDCard.initialize(this);
+    }
+
+    public static Context getContext() {
+        return mApp.getApplicationContext();
     }
 }
 
